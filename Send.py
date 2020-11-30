d5244504b0c966f2c115f8ccb806246af783f03d
jonl500 <jonl500@gmail.com>
2020-11-30 16:17:34 -0500

    SMTP email function

---
+++ Send.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+
+#recipients is a list of emails
+def relay(recipients, subject, message):
+	__port = 465
+	__password = 'qwerty357K!'
+	__sender = 'teamozattend@gmail.com'
+	#sender emails: teamozattend@gmail.com, ozattendance@gmail.com
+	
+	message = 'From: Team DevVolution <from@fromdomain.com>\nSubject: ' + str(subject) + '\n\n' + message
+	try:
+		with smtplib.SMTP_SSL('smtp.gmail.com', __port) as server:
+			server.login(__sender, __password)
+			server.sendmail(__sender, recipients, message)
+			server.quit()
+	except Exception as e:
+		print("Error: unable to send email")
+		print(e)
