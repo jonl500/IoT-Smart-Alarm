32ac39f6370e9b8fa8d6c9ec43a35c1bcb8963e8
jonl500 <45649952+jonl500@users.noreply.github.com>
2020-12-06 18:35:45 -0500

    Update Send.py

--- Send.py
+++ Send.py
@@ -1,5 +1,8 @@
 import smtplib
 import ssl
+import serial
+import time
+
 
 #recipients is a list of emails
 def relay(recipients, subject, message):
@@ -17,3 +20,22 @@ def relay(recipients, subject, message):
 	except Exception as e:
 		print("Error: unable to send email")
 		print(e)
+
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+time.sleep(2) # Need 2s delay after declaration of arduino
+arduino.write(b'51')
+stream = []
+j = 0
+
+while 1:
+	b = arduino.read_until(size=1)
+	if b == b'3':
+		if j == 0:
+			print("No spam?")
+			#relay(recipients = {recipients@here.place}, "Alarm system triggered", "Your alarm system has been triggered, call the cops and go home.")
+			j = 1
+	if b == b'4':
+		# relay(recipients = {recipients@here.place}, "Alarm system disabled", "Your alarm system has been disabled.")
+		print("No spam?")
+arduino.close()
